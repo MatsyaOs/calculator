fed5b6d8e4d0010ce98b1ee679322f5260b69ded
Abhishek Sharma <sabhiron5@gmail.com>
2022-05-14 10:45:07 +0530

    .

---
+++ build/CMakeFiles/cutefish-calculator.dir/DependInfo.cmake
@@ -0,0 +1,84 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/tokyo/clone/cuteclone/calculator/engine/number.c" "/home/tokyo/clone/cuteclone/calculator/build/CMakeFiles/cutefish-calculator.dir/engine/number.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_QMLMODELS_LIB"
+  "QT_QML_LIB"
+  "QT_QUICKCONTROLS2_LIB"
+  "QT_QUICK_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "cutefish-calculator_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQuick"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQmlModels"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQml"
+  "/usr/include/x86_64-linux-gnu/qt5/QtNetwork"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQuickControls2"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tokyo/clone/cuteclone/calculator/calcengine.cpp" "/home/tokyo/clone/cuteclone/calculator/build/CMakeFiles/cutefish-calculator.dir/calcengine.cpp.o"
+  "/home/tokyo/clone/cuteclone/calculator/build/cutefish-calculator_autogen/EWIEGA46WW/qrc_qml.cpp" "/home/tokyo/clone/cuteclone/calculator/build/CMakeFiles/cutefish-calculator.dir/cutefish-calculator_autogen/EWIEGA46WW/qrc_qml.cpp.o"
+  "/home/tokyo/clone/cuteclone/calculator/build/cutefish-calculator_autogen/mocs_compilation.cpp" "/home/tokyo/clone/cuteclone/calculator/build/CMakeFiles/cutefish-calculator.dir/cutefish-calculator_autogen/mocs_compilation.cpp.o"
+  "/home/tokyo/clone/cuteclone/calculator/engine/constants.cpp" "/home/tokyo/clone/cuteclone/calculator/build/CMakeFiles/cutefish-calculator.dir/engine/constants.cpp.o"
+  "/home/tokyo/clone/cuteclone/calculator/engine/evaluator.cpp" "/home/tokyo/clone/cuteclone/calculator/build/CMakeFiles/cutefish-calculator.dir/engine/evaluator.cpp.o"
+  "/home/tokyo/clone/cuteclone/calculator/engine/functions.cpp" "/home/tokyo/clone/cuteclone/calculator/build/CMakeFiles/cutefish-calculator.dir/engine/functions.cpp.o"
+  "/home/tokyo/clone/cuteclone/calculator/engine/hmath.cpp" "/home/tokyo/clone/cuteclone/calculator/build/CMakeFiles/cutefish-calculator.dir/engine/hmath.cpp.o"
+  "/home/tokyo/clone/cuteclone/calculator/main.cpp" "/home/tokyo/clone/cuteclone/calculator/build/CMakeFiles/cutefish-calculator.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_QMLMODELS_LIB"
+  "QT_QML_LIB"
+  "QT_QUICKCONTROLS2_LIB"
+  "QT_QUICK_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "cutefish-calculator_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQuick"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQmlModels"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQml"
+  "/usr/include/x86_64-linux-gnu/qt5/QtNetwork"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQuickControls2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cutefish-calculator_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cutefish-calculator_autogen"
+  "cutefish-calculator_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/cutefish-calculator_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
