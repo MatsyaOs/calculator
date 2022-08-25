8eff68a61c077675c96007185030bead522b4fbf
Abhishek Sharma <sabhiron5@gmail.com>
2022-08-25 10:31:01 +0530

    lol it's again

---
+++ build/CMakeFiles/matsya-calculator.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tokyo/matsyaos/matsyaos-dependencies/calculator/engine/number.c" "CMakeFiles/matsya-calculator.dir/engine/number.c.o" "gcc" "CMakeFiles/matsya-calculator.dir/engine/number.c.o.d"
+  "/home/tokyo/matsyaos/matsyaos-dependencies/calculator/calcengine.cpp" "CMakeFiles/matsya-calculator.dir/calcengine.cpp.o" "gcc" "CMakeFiles/matsya-calculator.dir/calcengine.cpp.o.d"
+  "/home/tokyo/matsyaos/matsyaos-dependencies/calculator/engine/constants.cpp" "CMakeFiles/matsya-calculator.dir/engine/constants.cpp.o" "gcc" "CMakeFiles/matsya-calculator.dir/engine/constants.cpp.o.d"
+  "/home/tokyo/matsyaos/matsyaos-dependencies/calculator/engine/evaluator.cpp" "CMakeFiles/matsya-calculator.dir/engine/evaluator.cpp.o" "gcc" "CMakeFiles/matsya-calculator.dir/engine/evaluator.cpp.o.d"
+  "/home/tokyo/matsyaos/matsyaos-dependencies/calculator/engine/functions.cpp" "CMakeFiles/matsya-calculator.dir/engine/functions.cpp.o" "gcc" "CMakeFiles/matsya-calculator.dir/engine/functions.cpp.o.d"
+  "/home/tokyo/matsyaos/matsyaos-dependencies/calculator/engine/hmath.cpp" "CMakeFiles/matsya-calculator.dir/engine/hmath.cpp.o" "gcc" "CMakeFiles/matsya-calculator.dir/engine/hmath.cpp.o.d"
+  "/home/tokyo/matsyaos/matsyaos-dependencies/calculator/main.cpp" "CMakeFiles/matsya-calculator.dir/main.cpp.o" "gcc" "CMakeFiles/matsya-calculator.dir/main.cpp.o.d"
+  "/home/tokyo/matsyaos/matsyaos-dependencies/calculator/build/matsya-calculator_autogen/EWIEGA46WW/qrc_qml.cpp" "CMakeFiles/matsya-calculator.dir/matsya-calculator_autogen/EWIEGA46WW/qrc_qml.cpp.o" "gcc" "CMakeFiles/matsya-calculator.dir/matsya-calculator_autogen/EWIEGA46WW/qrc_qml.cpp.o.d"
+  "/home/tokyo/matsyaos/matsyaos-dependencies/calculator/build/matsya-calculator_autogen/mocs_compilation.cpp" "CMakeFiles/matsya-calculator.dir/matsya-calculator_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/matsya-calculator.dir/matsya-calculator_autogen/mocs_compilation.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/matsya-calculator.dir/matsya-calculator_autogen/EWIEGA46WW/qrc_qml.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/matsya-calculator.dir/matsya-calculator_autogen/EWIEGA46WW/qrc_qml.cpp.o: \
+ /home/tokyo/matsyaos/matsyaos-dependencies/calculator/build/matsya-calculator_autogen/EWIEGA46WW/qrc_qml.cpp \
+ /usr/include/stdc-predef.h
